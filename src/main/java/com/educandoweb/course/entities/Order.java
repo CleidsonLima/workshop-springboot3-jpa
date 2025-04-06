a1b8787b3bce3c75e4c48a9fe52c2aa723e357bb
CleidsonLima <cleidson_lima@outlook.com>
2025-04-06 13:20:33 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -3,6 +3,7 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -26,6 +27,7 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMZ")
 	private Instant moment;
 	
+	private Integer orderstatus;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -35,10 +37,11 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderstatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderstatus(orderstatus); 
 		this.client = client;
 	}
 
@@ -56,6 +59,17 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+		
+	public OrderStatus getOrderstatus() {
+		return OrderStatus.valueOf(orderstatus);
+	}
+
+	public void setOrderstatus(OrderStatus orderstatus) {
+		if(orderstatus!= null){
+		this.orderstatus = orderstatus.getCode();
+		}
 	}
 
 	public User getClient() {
